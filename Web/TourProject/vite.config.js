5e74ead7c19f8fa0c6cf8156f946f0844dd66b97
NguyenVanLoc2002 <nguyenvanloc6756@gmail.com>
2024-12-08 15:48:25 +0700

    add static.json

--- Web/TourProject/vite.config.js
+++ Web/TourProject/vite.config.js
@@ -10,4 +10,18 @@ export default defineConfig({
       '@': resolve(__dirname, 'src'),
     },
   },
+  server: {
+    port: 3000, // Cổng bạn sử dụng (tuỳ chỉnh nếu cần)
+  },
+  build: {
+    outDir: 'dist', // Thư mục build (mặc định là 'dist')
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom'],
+  },
+  preview: {
+    headers: {
+      'Cache-Control': 'public, max-age=3600',
+    },
+  },
 })
